4152af05238ae9b9a16416d8e5552b64229ec68c
Anna <anysofronova@yandex.ru>
2022-07-12 12:08:14 +0300

    Update Event

--- src/components/EventList/Event/Event.tsx
+++ src/components/EventList/Event/Event.tsx
@@ -1,11 +1,12 @@
 import styles from "./Event.module.scss";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { IEvent } from "../../../@types/IEvent";
 import placeholder from "../../../assets/imgPlaceholder.jpeg";
 import { clsx } from "clsx";
 import { useAppDispatch } from "../../../hooks/redux";
-import { deleteEvent } from "../../../store/mainSlice";
+import { deleteEvent, updateEvent } from "../../../store/mainSlice";
 import moment from "moment";
+import Form from "../../AddEvent/Form/Form";
 
 const Event: FC<IEvent> = ({
   image,
@@ -17,26 +18,41 @@ const Event: FC<IEvent> = ({
   years,
 }) => {
   const dispatch = useAppDispatch();
-
+  const [editMode, setEditMode] = useState<boolean>(false);
   return (
-    <div className={clsx(styles.event)}>
-      <div className={styles.image}>
-        <img src={image || placeholder} alt="Event" />
-      </div>
-      <div className={styles.info}>
-        <div className={styles.buttons}>
-          <button onClick={() => dispatch(deleteEvent(id))}>╳</button>
+    <>
+      {!editMode && (
+        <div className={clsx(styles.event)}>
+          <div className={styles.image}>
+            <img src={image || placeholder} alt="Event" />
+          </div>
+          <div className={styles.info}>
+            <div className={styles.buttons}>
+              <button onClick={() => setEditMode(true)}>Edit</button>
+              <button onClick={() => dispatch(deleteEvent(id))}>╳</button>
+            </div>
+            <div className={styles.type}>{type}</div>
+            <div className={styles.name}>
+              {name} – {moment(date).format("MMMM Do YYYY")}
+            </div>
+            <div className={styles.date}>{daysBefore}</div>
+            {type === "Birthday" && (
+              <div className={styles.date}>Will be {years} years old</div>
+            )}
+          </div>
         </div>
-        <div className={styles.type}>{type}</div>
-        <div className={styles.name}>
-          {name} – {moment(date).format("MMMM Do YYYY")}
+      )}
+      {editMode && (
+        <div className={clsx(styles.edit, styles.event)}>
+          <Form
+            buttonText={"Update"}
+            dispatchName={updateEvent}
+            eventInfo={{ name, type, image, id, date }}
+            setEditMode={setEditMode}
+          />
         </div>
-        <div className={styles.date}>{daysBefore}</div>
-        {type === "Birthday" && (
-          <div className={styles.date}>Will be {years} years old</div>
-        )}
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
