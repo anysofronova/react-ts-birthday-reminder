f8f8a551e3e56b73dc2b1e70de3cd9e2730f7c8c
Anna <anysofronova@yandex.ru>
2022-07-17 09:08:30 +0300

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AddEvent from "./components/AddEvent/AddEvent";
 import Header from "./components/Header/Header";
 import EventList from "./components/EventList/EventList";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <AddEvent />
       <EventList />
+      <Footer />
     </div>
   );
 }
